fd02b0007e3cd269d9d62250933e9897556da70c
rsullivan00 <rick@ricksullivan.net>
2020-07-21 22:08:02 -0700

    Use transfer learning, results still terrible

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import numpy as np
 
 import os
 
-image_size = (512, 512)
-batch_size = 8
+image_size = (150, 150)
+input_shape = image_size + (3,)
+batch_size = 32
 image_dir = "original-images"
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -36,85 +38,82 @@ data_augmentation = keras.Sequential(
     ]
 )
 
+# https://keras.io/guides/transfer_learning/
+base_model = keras.applications.Xception(
+    weights="imagenet",  # Load weights pre-trained on ImageNet.
+    input_shape=input_shape,
+    include_top=False,
+)
 
-def make_model(input_shape, num_classes):
-    inputs = keras.Input(shape=input_shape)
-    # Image augmentation block
-    x = data_augmentation(inputs)
+base_model.trainable = False
 
-    # Entry block
-    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
-    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
+inputs = keras.Input(shape=input_shape)
+x = data_augmentation(inputs)
+# Pre-trained Xception weights requires that input be normalized
+# from (0, 255) to a range (-1., +1.), the normalization layer
+# does the following, outputs = (inputs - mean) / sqrt(var)
+norm_layer = keras.layers.experimental.preprocessing.Normalization()
+mean = np.array([127.5] * 3)
+var = mean ** 2
+# Scale inputs to [-1, +1]
+x = norm_layer(x)
+norm_layer.set_weights([mean, var])
 
-    x = layers.Conv2D(64, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    previous_block_activation = x  # Set aside residual
-
-    for size in [128, 256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.GlobalAveragePooling2D()(x)
-    if num_classes == 2:
-        activation = "sigmoid"
-        units = 1
-    else:
-        activation = "softmax"
-        units = num_classes
-
-    x = layers.Dropout(0.5)(x)
-    outputs = layers.Dense(units, activation=activation)(x)
-    return keras.Model(inputs, outputs)
-
-
-model = make_model(input_shape=image_size + (3,), num_classes=2)
+# The base model contains batchnorm layers. We want to keep them in inference mode
+# when we unfreeze the base model for fine-tuning, so we make sure that the
+# base_model is running in inference mode here.
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
 keras.utils.plot_model(model, show_shapes=True)
 
-epochs = 50
+epochs = 10
 
-callbacks = [
-    keras.callbacks.ModelCheckpoint(".checkpoints/save_at_{epoch}.h5"),
-]
 model.compile(
     optimizer=keras.optimizers.Adam(1e-3),
-    loss="binary_crossentropy",
-    metrics=["accuracy"],
-)
-history = model.fit(
-    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
 )
 
+
+first_fit = model.fit(train_ds, epochs=epochs, validation_data=val_ds,)
+
+# Unfreeze the base_model. Note that it keeps running in inference mode
+# since we passed `training=False` when calling it. This means that
+# the batchnorm layers will not update their batch statistics.
+# This prevents the batchnorm layers from undoing all the training
+# we've done so far.
+base_model.trainable = True
+model.summary()
+
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-5),  # Low learning rate
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+epochs = 10
+final_fit = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
+
+train_accuracy_history = (
+    first_fit.history["binary_accuracy"] + final_fit.history["binary_accuracy"]
+)
+eval_accuracy_history = (
+    first_fit.history["val_binary_accuracy"] + final_fit.history["val_binary_accuracy"]
+)
 
 import matplotlib.pyplot as plt
 
-plt.plot(range(1, epochs + 1), history.history["accuracy"], label="train")
-plt.plot(range(1, epochs + 1), history.history["val_accuracy"], label="eval")
+
+plt.plot(
+    range(1, len(train_accuracy_history) + 1), train_accuracy_history, label="train"
+)
+plt.plot(range(1, len(eval_accuracy_history) + 1), eval_accuracy_history, label="eval")
 plt.xlabel("Epochs")
 plt.ylabel("Accuracy")
 plt.legend()
 
 plt.savefig("accuracy.png")
-
 plt.show()
