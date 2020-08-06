39ddd2ec3e19408b8c3fe9639c20e2e4245e244a
rsullivan00 <rick@ricksullivan.net>
2020-08-05 22:25:03 -0700

    Try EfficientNetB0

--- model.py
+++ model.py
@@ -2,10 +2,11 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
+import matplotlib.pyplot as plt
 
 import os
 
-image_size = (140, 140)
+image_size = (224, 224)
 input_shape = image_size + (3,)
 batch_size = 32
 image_dir = "all-images"
@@ -38,83 +39,78 @@ data_augmentation = keras.Sequential(
     ]
 )
 
-# https://keras.io/guides/transfer_learning/
-# NASNetMobile looks good
-base_model = keras.applications.NASNetMobile(
-    weights="imagenet",  # Load weights pre-trained on ImageNet.
-    input_shape=input_shape,
-    include_top=False,
-)
 
-base_model.trainable = False
-
-inputs = keras.Input(shape=input_shape)
-x = data_augmentation(inputs)
-# Pre-trained Xception weights requires that input be normalized
-# from (0, 255) to a range (-1., +1.), the normalization layer
-# does the following, outputs = (inputs - mean) / sqrt(var)
-norm_layer = keras.layers.experimental.preprocessing.Normalization()
-mean = np.array([127.5] * 3)
-var = mean ** 2
-# Scale inputs to [-1, +1]
-x = norm_layer(x)
-norm_layer.set_weights([mean, var])
-
-# The base model contains batchnorm layers. We want to keep them in inference mode
-# when we unfreeze the base model for fine-tuning, so we make sure that the
-# base_model is running in inference mode here.
-x = base_model(x, training=False)
-x = keras.layers.GlobalAveragePooling2D()(x)
-x = keras.layers.Dropout(0.6)(x)  # Regularize with dropout
-outputs = keras.layers.Dense(1)(x)
-model = keras.Model(inputs, outputs)
-keras.utils.plot_model(model, show_shapes=True)
-
-epochs = 20
-
-model.compile(
-    optimizer=keras.optimizers.Adam(1e-3),
-    loss=keras.losses.BinaryCrossentropy(from_logits=True),
-    metrics=[keras.metrics.BinaryAccuracy()],
-)
+fig_count = 0
 
 
-first_fit = model.fit(train_ds, epochs=epochs, validation_data=val_ds,)
-
-# Unfreeze the base_model. Note that it keeps running in inference mode
-# since we passed `training=False` when calling it. This means that
-# the batchnorm layers will not update their batch statistics.
-# This prevents the batchnorm layers from undoing all the training
-# we've done so far.
-base_model.trainable = True
-model.summary()
-
-model.compile(
-    optimizer=keras.optimizers.Adam(1e-5),  # Low learning rate
-    loss=keras.losses.BinaryCrossentropy(from_logits=True),
-    metrics=[keras.metrics.BinaryAccuracy()],
-)
-
-epochs = 2
-final_fit = model.fit(train_ds, epochs=epochs, validation_data=val_ds)
-
-train_accuracy_history = (
-    first_fit.history["binary_accuracy"] + final_fit.history["binary_accuracy"]
-)
-eval_accuracy_history = (
-    first_fit.history["val_binary_accuracy"] + final_fit.history["val_binary_accuracy"]
-)
-
-import matplotlib.pyplot as plt
+def plot_hist(hist):
+    global fig_count
+    fig_count += 1
+    plt.figure(fig_count)
+    plt.plot(hist.history["accuracy"])
+    plt.plot(hist.history["val_accuracy"])
+    plt.title("model accuracy")
+    plt.ylabel("accuracy")
+    plt.xlabel("epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.savefig("{}.png".format(fig_count))
 
 
-plt.plot(
-    range(1, len(train_accuracy_history) + 1), train_accuracy_history, label="train"
-)
-plt.plot(range(1, len(eval_accuracy_history) + 1), eval_accuracy_history, label="eval")
-plt.xlabel("Epochs")
-plt.ylabel("Accuracy")
-plt.legend()
+# https://keras.io/examples/vision/image_classification_efficientnet_fine_tuning/
+def build_model(num_classes, input_shape):
+    inputs = layers.Input(shape=input_shape)
+    x = data_augmentation(inputs)
+    model = keras.applications.EfficientNetB0(
+        include_top=False, input_tensor=x, weights="imagenet"
+    )
 
-plt.savefig("accuracy.png")
+    # Freeze the pretrained weights
+    model.trainable = False
+
+    # Rebuild top
+    x = layers.GlobalAveragePooling2D(name="avg_pool")(model.output)
+    x = layers.BatchNormalization()(x)
+
+    top_dropout_rate = 0.4
+    x = layers.Dropout(top_dropout_rate, name="top_dropout")(x)
+    outputs = layers.Dense(num_classes, activation="softmax", name="pred")(x)
+
+    # Compile
+    model = tf.keras.Model(inputs, outputs, name="EfficientNet")
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
+    model.compile(
+        optimizer=optimizer,
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics=["accuracy"],
+    )
+    return model
+
+
+model = build_model(num_classes=2, input_shape=input_shape)
+
+epochs = 10  # @param {type: "slider", min:8, max:80}
+hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds, verbose=2)
+plot_hist(hist)
+
+
+def unfreeze_model(model):
+    # We unfreeze the top 20 layers while leaving BatchNorm layers frozen
+    for layer in model.layers[-20:]:
+        if not isinstance(layer, layers.BatchNormalization):
+            layer.trainable = True
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    model.compile(
+        optimizer=optimizer,
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics=["accuracy"],
+    )
+
+
+unfreeze_model(model)
+
+epochs = 10  # @param {type: "slider", min:8, max:50}
+hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds, verbose=2)
+plot_hist(hist)
+
 plt.show()
